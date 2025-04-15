841be2977700ad1c5533e1225fd69add86dcc547
wtpbell <78562238+wtpbell@users.noreply.github.com>
2025-04-15 18:42:56 +0200

    add ex02

---
+++ cpp02/ex02/include/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bewong <bewong@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/15 17:24:05 by bewong        #+#    #+#                 */
+/*   Updated: 2025/04/15 17:25:46 by bewong        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+			int					_fixedPoint;
+			static const int	_fractionalBits = 8;
+
+	public:
+			Fixed();
+			Fixed(const int value);
+			Fixed(const float value);
+			Fixed(const Fixed& other);
+			Fixed& operator=(const Fixed& other);
+			~Fixed();
+
+			int		getRawBits(void) const;
+			void	setRawBits(int const raw);
+			float	toFloat(void) const;
+			int		toInt(void) const;
+		};
+		
+	std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
