f45e2cd762add453c6b91b621de313f9c04a198c
Bell Wong <78562238+wtpbell@users.noreply.github.com>
2025-09-28 17:43:46 +0200

    added pairup and binarySearch functions

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -15,6 +15,8 @@
 #include <string>
 #include <algorithm>
 
+// https://medium.com/@ryan_forrester_/binary-sort-in-c-complete-guide-0afcb29c9aec
+
 bool	isAllDigits(std::string_view str)
 {
 	return (!str.empty() && std::all_of(str.begin(), str.end(), ::isdigit));
@@ -52,6 +54,73 @@ std::vector<int>	handleVector(int size, char** argv)
 	return (v);
 }
 
+std::vector<std::pair<int, int>>	pairup(std::vector<int>& v)
+{
+	std::vector<std::pair<int, int>> pairs;
+	int					solo = 0;
+	bool				is_odd = false;
+
+	std::cout << "Before:";
+	for (auto num : v)
+		std::cout << " " << num;
+	std::cout << std::endl;
+
+	if (v.size() % 2 != 0)
+	{
+		is_odd = true;
+		solo = v.back();
+		v.pop_back();
+	}
+
+	for (size_t i = 0; i < v.size(); i += 2)
+	{
+		if (i + 1 < v.size())
+			pairs.emplace_back(std::make_pair(v[i], v[i + 1]));
+	}
+
+	std::cout << "After: ";
+	for (const auto& pair : pairs)
+		std::cout << "(" << pair.first << ", " << pair.second << ") ";
+	if (is_odd)
+		std::cout << solo << " (unpaired)";
+	std::cout << std::endl;
+
+	v.clear();
+	for (const auto& pair : pairs)
+	{
+		v.emplace_back(pair.first);
+		v.emplace_back(pair.second);
+	}
+	if (is_odd)
+		v.emplace_back(solo);
+	return (pairs);
+}
+
+void	swapPair(std::vector<std::pair<int, int>>& v)
+{
+	for (auto& [first, second] : v)
+		if (first > second)
+			std::swap(first, second);
+	std::cout << "After: ";
+	for (const auto& pair : v)
+		std::cout << "(" << pair.first << ", " << pair.second << ") ";
+	std::cout << std::endl;
+}
+
+size_t	binarySearch(const std::vector<int>& v, int target, size_t left, size_t right)
+{
+	while (left < right)
+	{
+		size_t mid = left + (right - left) / 2;
+		if (v[mid] == target)
+			return (mid);
+		if (v[mid] < target)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	return (left);
+}
 
 int	main(int argc, char** argv)
 {
@@ -70,6 +139,7 @@ int	main(int argc, char** argv)
 	{
 		return (1);
 	}
-
+	auto pairs = pairup(vect);
+	swapPair(pairs);
 	return (0);
 }
